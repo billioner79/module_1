02a971093a569dfbc2f1bb245816b4764078b72e
dkolbin <udm.mts@gmail.com>
2024-12-15 20:39:50 +0400

    Init

---
+++ main.py
@@ -0,0 +1,7 @@
+my_string = input("Назовите Ваше полное имя (ФИО) ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[0])
+print(my_string[-1])
